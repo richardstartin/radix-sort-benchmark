24c175fc2820ddc4d49a6f9cfb0653d29dfdbe28
richardstartin <richard@openkappa.co.uk>
2021-04-11 10:35:29 +0100

    add more data generators

--- src/main/java/io/github/richardstartin/radixsort/DataScenario.java
+++ src/main/java/io/github/richardstartin/radixsort/DataScenario.java
@@ -15,7 +15,7 @@ public enum DataScenario {
       return data;
     }
   },
-  SORTED {
+  CONTIGUOUS {
     @Override
     int[] generate(int size, int seed, int mask) {
       int[] data = new int[size];
@@ -25,12 +25,48 @@ public enum DataScenario {
       return data;
     }
   },
-  ALMOST_SORTED {
+  CONTIGUOUS_REVERSE {
+    @Override
+    int[] generate(int size, int seed, int mask) {
+      int[] data = new int[size];
+      for (int i = 0; i < size; ++i) {
+        data[i] = size - i;
+      }
+      return data;
+    }
+  },
+  ALMOST_CONTIGUOUS {
     @Override
     int[] generate(int size, int seed, int mask) {
       int[] data = new int[size];
       for (int i = 0; i < data.length; ++i) {
         data[i] = (i ^ 0xFF) & mask;
+      }
+      return data;
+    }
+  },
+  SORTED {
+    @Override
+    int[] generate(int size, int seed, int mask) {
+      SplittableRandom random = new SplittableRandom(seed);
+      int[] data = new int[size];
+      int x = (int) (Math.log(random.nextDouble())/Math.log(0.999)) + 1;
+      for (int i = 0; i < size; ++i) {
+        data[i] = x & mask;
+        x += (int) (Math.log(random.nextDouble())/Math.log(0.999)) + 1;
+      }
+      return data;
+    }
+  },
+  ALMOST_SORTED {
+    @Override
+    int[] generate(int size, int seed, int mask) {
+      int[] data = new int[size];
+      SplittableRandom random = new SplittableRandom(seed);
+      int x = (int) (Math.log(random.nextDouble())/Math.log(0.999)) + 1;
+      for (int i = 0; i < size; ++i) {
+        data[i] = (x ^ 0xFF) & mask;
+        x += (int) (Math.log(random.nextDouble())/Math.log(0.999)) + 1;
       }
       return data;
     }
